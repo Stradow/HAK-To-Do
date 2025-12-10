dd348ed8fb61c2714a28e149da1afd56ce54e082
Amaury Lesigne <lesigne.amaury@gmail.com>
2025-12-10 17:59:25 +0100

    adding Json/task/checkbox

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
+import dailyTasks from "../data.json";
+
 function Body() {
+  const [tasks, setTasks] = useState(dailyTasks);
+
+  function checkBoxChange(taskId) {
+    const updatedTasks = tasks.map((oneTask) => {
+      if (oneTask.id === taskId) {
+        return {
+          id: oneTask.id,
+          text: oneTask.text,
+          isDone: !oneTask.isDone,
+        };
+      }
+      return oneTask;
+    });
+    setTasks(updatedTasks);
+  }
+  function handleDeleteTask(taskId) {
+    const updatedTasks = tasks.filter((oneTask) => {
+      return oneTask.id !== taskId;
+    });
+  }
+
   return (
     <div className="body">
-      <h2>this is the body</h2>
-      <p>
-        item details page (list of task that day) delete items (button to
-        remouve task) edit item (button to edit task) conditional rendering
-        (button to mark task complete)
-      </p>
+      {tasks.map((oneTask) => (
+        <div id="daily-task-container" key={oneTask.id}>
+          <input
+            type="checkbox"
+            checked={oneTask.isDone}
+            onChange={() => checkBoxChange(oneTask.id)}
+          />
+          <span>{oneTask.text}</span>
+
+          <button onClick={() => handleDeleteTask(oneTask.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
