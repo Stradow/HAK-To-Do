ca48a3dbfe7b61ae8683737e9044ed8c3462c382
Hamza Mahmood <hamzamahmood195@gmail.com>
2025-12-15 16:36:30 +0000

    adding a task functionality

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,8 @@ import dailyTasks from "../data.json";
 function Body({ selectedDay }) {
   const [tasks, setTasks] = useState(dailyTasks);
 
+  const [newTaskText, setNewTaskText] = useState("");
+
   function checkBoxChange(taskId) {
     const updatedTasks = tasks.map((oneTask) => {
       if (oneTask.id === taskId) {
@@ -30,6 +32,24 @@ function Body({ selectedDay }) {
     return oneTask.days.includes(selectedDay);
   });
 
+  function handleAddTask(event) {
+    event.preventDefault();
+
+    if (newTaskText.trim() === "") {
+      return;
+    }
+
+    const newTask = {
+      id: Date.now(),
+      text: newTaskText,
+      isDone: false,
+      days: [selectedDay],
+    };
+
+    setTasks([newTask, ...tasks]);
+    console.log(setTasks);
+  }
+
   return (
     <div className="body">
       {visibleTasks.map((oneTask) => (
@@ -44,6 +64,22 @@ function Body({ selectedDay }) {
           <button onClick={() => handleDeleteTask(oneTask.id)}>Delete</button>
         </div>
       ))}
+      <form onSubmit={handleAddTask}>
+        <label>
+          Add task:
+          <input
+            type="text"
+            name="task"
+            value={newTaskText}
+            onChange={(event) => {
+              // handleAddTask(event.target.value);
+              setNewTaskText(event.target.value);
+            }}
+            placeholder="Add your new task here"
+          ></input>
+        </label>
+        <button onClick={handleAddTask}>Add</button>
+      </form>
     </div>
   );
 }
